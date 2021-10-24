70a0a37214a921ee82a4901badbfa28c274648c2
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-10-24 23:08:31 +0200

    Estruturando master para merge

--- src/pages/Home/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-    font-size: 10rem;
-    text-align: left;
-    width: 100%;
-    margin: 0 20px;
-    color: #fff;
-`
