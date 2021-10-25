245e57c8de5ddf3abf1398bd9ef56f4b05b94dc5
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-10-26 00:09:04 +0200

    Added loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { AppProps } from "next/app"
-
+import { Router } from "next/router";
+import ReactLoading from 'react-loading';
 import Header from '../components/Header'
+import NProgress from 'nprogress'; //nprogress module
 
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
+
   return (
     <>
       <Header />
+      {/* <ReactLoading type={'balls'} color={'yellow'} height={'20%'} width={'20%'} /> */}
       <Component {...pageProps} />
     </>
     )
