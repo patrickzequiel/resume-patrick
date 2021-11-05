5af6852a471b20083c8076d60bb47a90b6037f9f
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-11-05 15:22:44 +0100

    Changed footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,15 +10,22 @@ export function Footer() {
         <>
             <footer className={styles.footer}>
                 <div className={styles.footer__social}>
-                    <a href="">
-                        <FaInstagram size="20" color="#fff" />
-                    </a>
-                    <a href="">
-                        <AiFillLinkedin size="20" color="#fff" />
-                    </a>
-                    <a href="">
-                        <FaGithubSquare size="20" color="#fff" />
-                    </a>
+                    <div className={styles.footer__social__name}>
+                        <p>
+                            Created by Patrick / made with NextJs
+                        </p>
+                    </div>
+                    <div className={styles.footer__social__icons}>
+                        <a className={styles.icon}href="https://www.instagram.com/patescruz/">
+                            <FaInstagram size="35" color="#fff" />
+                        </a>
+                        <a className={styles.icon} href="https://www.linkedin.com/in/patrickzequiel/">
+                            <AiFillLinkedin size="35" color="#fff" />
+                        </a>
+                        <a className={styles.icon} href="https://github.com/patrickzequiel">
+                            <FaGithubSquare size="35" color="#fff" />
+                        </a>
+                    </div>
                 </div>
             </footer>
         </>
